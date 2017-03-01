1d5d1d7cf12f7c55cc336664bce2ea069ec8761c
marie-helene.rots <marie-helene.rots@orange.fr>
2017-03-01 13:03:17 +0100

    cities

--- client/navbar.js
+++ client/navbar.js
@@ -1,5 +1,7 @@
 Template.navbar.events({
    'click #reset': function() {
-       Meteor.call('reset');
+       Meteor.call('reset', function (err, res) {
+           if (err) console.log("!!", err);
+       });
    } 
 });
